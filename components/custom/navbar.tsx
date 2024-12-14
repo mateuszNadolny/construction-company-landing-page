6a2364df66ff3c0a7776272614820dd4cd0f03b5
mateuszNadolny <mateusznadolny00@gmail.com>
2024-12-14 16:11:56 +0100

    refactor: Replace scroll direction hook with mouse activity detection for Navbar visibility
    - Removed useScrollDirection hook and implemented useMouseActivity hook to manage Navbar visibility based on mouse movement.
    - Updated Navbar styling and structure, including a new logo and improved layout for better user experience.
    - Enhanced responsiveness and visual appeal of the Navbar component.

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { motion, AnimatePresence } from "framer-motion";
-import useScrollDirection from "@/hooks/useScrollDirection";
+import useMouseActivity from "@/hooks/useMouseActivity";
+import { Bolt } from "lucide-react";
 
 const Navbar = () => {
-  const isVisible = useScrollDirection();
+  const isVisible = useMouseActivity();
 
   const handleNavClick = (sectionId: string) => {
     const element = document.getElementById(sectionId);
@@ -18,10 +19,24 @@ const Navbar = () => {
           animate={{ y: 0 }}
           exit={{ y: -100 }}
           transition={{ duration: 0.3 }}
-          className="hidden sm:block fixed top-5 left-0 right-0 lg:right-40 bg-transparent z-50"
+          className="hidden sm:block fixed w-full top-0 left-0 bg-blend-color-dodge z-50 bg-white/90 backdrop-blur-md"
         >
-          <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-end items-center h-16 gap-8">
+          <div className="max-w-full flex justify-between items-center px-4 sm:px-6 lg:px-12">
+            <div className="flex items-center gap-2 p-2">
+              <Bolt className="w-6 h-6 text-yellow-400 font-[900]" />
+              <p className="text-xl font-extrabold text-slate-900 tracking-tighter">
+                Constructex
+              </p>
+            </div>
+            <div className="flex w-full justify-end items-center h-16 gap-8">
+              <button
+                onClick={() => handleNavClick("about")}
+                className="text-slate-900 hover:underline px-3 py-2 rounded-md text-sm font-medium transition-colors focus:outline-none"
+                aria-label="Navigate to Our Work section"
+                tabIndex={0}
+              >
+                Our mission
+              </button>
               <button
                 onClick={() => handleNavClick("testimonials")}
                 className="text-slate-900 hover:underline px-3 py-2 rounded-md text-sm font-medium transition-colors focus:outline-none"
