1a0de43a213f2c15c5487a53913aed7bdf2d7803
mateuszNadolny <mateusznadolny00@gmail.com>
2024-12-17 18:58:30 +0100

    feat: Enhance ProjectsGrid component with scroll-based animations
    - Integrated useRef and useInView hooks for scroll visibility detection.
    - Updated motion animations for the ProjectsGrid to improve user experience.
    - Added staggered animations for project items to enhance visual appeal.
    - Refactored initial animation logic to utilize variants for better maintainability.

--- components/custom/projects-grid.tsx
+++ components/custom/projects-grid.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useState, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import ProjectCard from "@/components/custom/project-card";
 import { projects } from "@/lib/data";
 import { Flower } from "lucide-react";
@@ -8,33 +8,62 @@ import { Flower } from "lucide-react";
 const ProjectsGrid = () => {
   const [hoveredId, setHoveredId] = useState<number | null>(null);
 
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, margin: "-100px" });
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.6,
+        staggerChildren: 0.125,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 200 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5 },
+    },
+  };
+
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6 }}
+      ref={ref}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      variants={containerVariants}
       className="z-20 w-[90%] lg:w-[70%]"
     >
-      <motion.h2 className="text-slate-950 text-transparent text-3xl font-bold tracking-tighter sm:text-4xl bg-gradient-to-r from-gray-900 to-gray-700 bg-clip-text  px-4 mb-2">
+      <motion.h2
+        variants={itemVariants}
+        className="text-slate-950 text-transparent text-3xl font-bold tracking-tighter sm:text-4xl bg-gradient-to-r from-gray-900 to-gray-700 bg-clip-text px-4 mb-2"
+      >
         Explore our recent work
       </motion.h2>
-      <motion.h3 className="text-muted-foreground text-sm md:text-lg mb-8 px-4 flex items-start md:items-center gap-2">
+      <motion.h3
+        variants={itemVariants}
+        className="text-muted-foreground text-sm md:text-lg mb-8 px-4 flex items-start md:items-center gap-2"
+      >
         <Flower className="w-6 h-6 text-yellow-400" />
         Each project reflects our commitment to quality and design excellence.
       </motion.h3>
       <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 px-4 max-w-7xl mx-auto w-full "
+        variants={containerVariants}
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 px-4 max-w-7xl mx-auto w-full"
       >
         {projects.map((project) => (
-          <ProjectCard
-            key={project.id}
-            project={project}
-            hoveredId={hoveredId}
-            setHoveredId={setHoveredId}
-          />
+          <motion.div key={project.id} variants={itemVariants}>
+            <ProjectCard
+              project={project}
+              hoveredId={hoveredId}
+              setHoveredId={setHoveredId}
+            />
+          </motion.div>
         ))}
       </motion.div>
     </motion.div>
